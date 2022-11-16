0256914adf3fdd783d73e6f8ad60bea8b4808289
Anton Grevtsov <grevtsov.apn@gmail.com>
2022-11-16 21:11:37 +0700

    maybe ready maybe not

---
+++ src/cat/check/utils.h
@@ -0,0 +1,11 @@
+/* This file was automatically generated.  Do not edit! */
+#undef INTERFACE
+void print_error_illegal_option(char *argv[],int invalid_len,char *invalid_opt);
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#define BIN_NAME (argv[0] + 2)
+void print_error_no_file(char *argv[],int file_ndx);
